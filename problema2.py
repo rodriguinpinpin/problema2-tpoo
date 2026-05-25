4347b6515ee0ab20f40bb2eb0f13cd346f537379
rodriguinpinpin <amner.cabada@gmail.com>
2026-05-25 18:44:44 -0500

    USAMOS LA LISTA Y SUMA RECURSIVA

--- problema2.py
+++ problema2.py
@@ -1,3 +1,12 @@
 def sumar_recursiva(lista, PI, PF):
     if PI > PF:
         return 0
+    else:
+        return lista[PI] + sumar_recursiva(lista, PI + 1, PF)
+
+    lista = [2, 4, 6, 3]
+    pos_inicial = 2
+    pos_final = 3
+    
+    
+    
